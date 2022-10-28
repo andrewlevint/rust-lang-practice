2b5cf6f54f2eba450e2a4d14b63eddb92eb480e2
Andrew Levin <andrewlevingit@hotmail.com>
2022-10-28 22:51:23 +0400

    5

--- project_cargo/target/debug/deps/project_cargo-1a4e2e2336901d95.d
+++
@@ -1,5 +0,0 @@
-/Users/andrewlevin/Desktop/rust-lang-practice/project_cargo/target/debug/deps/project_cargo-1a4e2e2336901d95.rmeta: src/main.rs
-
-/Users/andrewlevin/Desktop/rust-lang-practice/project_cargo/target/debug/deps/project_cargo-1a4e2e2336901d95.d: src/main.rs
-
-src/main.rs:
--- project_cargo/target/debug/deps/project_cargo-8f51b7bc7ba1f5eb.d
+++
@@ -1,5 +0,0 @@
-/Users/andrewlevin/Desktop/rust-lang-practice/project_cargo/target/debug/deps/project_cargo-8f51b7bc7ba1f5eb: src/main.rs
-
-/Users/andrewlevin/Desktop/rust-lang-practice/project_cargo/target/debug/deps/project_cargo-8f51b7bc7ba1f5eb.d: src/main.rs
-
-src/main.rs:
